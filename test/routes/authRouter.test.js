b770b7e8407e389f6c5578b98b0e43eade7e7215
Matthew Thorin <mthorin@byu.edu>
2024-10-01 18:14:12 -0600

    added test 'login fail'

--- test/routes/authRouter.test.js
+++ test/routes/authRouter.test.js
@@ -1,5 +1,30 @@
+const request = require('supertest');
+const app = require('../../src/service');
 
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
 
-test('hello world', () => {
-    expect('hello' + ' world').toBe('hello world');
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
 });
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('return 400 for invalid login credentials', async () => {
+    const response = await request(app)
+        .put('/api/auth')
+        .send({ name: 'invalid', email: 'invalid@jwt.com', password: 'wrongpassword' });
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('unknown user');
+});
+
